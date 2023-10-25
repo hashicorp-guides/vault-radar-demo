5931d1ae3cf4aa14d0af4c1a6f3c19528e95b783
Annalise Diroff <adiroff@hashicorp.com>
2023-10-25 13:42:40 -0700

    Adding count

--- aws/main.tf
+++ aws/main.tf
@@ -17,30 +17,46 @@ provider "aws" {
   region = "us-east-2"
 }
 
+// Github Pat Tokens
 resource "aws_ssm_parameter" "github_pat" {
-  name  = join("/", [local.name_prefix, "github/pat", ])
+  name  = join("/", [local.name_prefix, "github/pat${count.index + 1}", ])
   type  = "String"
-  value = local.yaml_secrets.samples.github_pats[0]
+  count = 2
+  value = local.yaml_secrets.samples.github_pats[count.index]
 }
 
+// AWS Creds
 resource "aws_ssm_parameter" "aws_secret_creds" {
-  name  = join("/", [local.name_prefix, "secret/creds", ])
+  name  = join("/", [local.name_prefix, "secret/creds${count.index + 1}", ])
   type  = "String"
-  value = "aws_access_key_id=${local.yaml_secrets.samples.aws[0].aws_access_key_id} aws_secret_access_key=${local.yaml_secrets.samples.aws[0].aws_secret_key}"
+  count = 3
+  value = "aws_access_key_id=${local.yaml_secrets.samples.aws[count.index].aws_access_key_id} aws_secret_access_key=${local.yaml_secrets.samples.aws[count.index].aws_secret_key}"
 }
 
+// Vault Tokens
 resource "aws_ssm_parameter" "vault_token" {
-  name  = join("/", [local.name_prefix, "vault/token", ])
+  name  = join("/", [local.name_prefix, "vault/token${count.index + 1}", ])
   type  = "String"
-  value = local.yaml_secrets.samples.vault_token[0]
+  count = 2
+  value = local.yaml_secrets.samples.vault_token[count.index]
 }
 
+// Gitlab Pat Tokens
+resource "aws_ssm_parameter" "gitlab_pats" {
+  name  = join("/", [local.name_prefix, "gitlab/pats${count.index + 1}", ])
+  type  = "String"
+  count = 2
+  value = local.yaml_secrets.samples.gitlab_pats[count.index]
+}
+
+// Slack tokens as a secret list
 resource "aws_ssm_parameter" "slack_secret_list" {
   name  = join("/", [local.name_prefix, "slack/secret/list", ])
   type  = "StringList"
-  value = join(",", [local.yaml_secrets.samples.slack_secrets[0], local.yaml_secrets.samples.slack_secrets[0], ])
+  value = join(",", [local.yaml_secrets.samples.slack_secrets[0], local.yaml_secrets.samples.slack_secrets[1], ])
 }
 
+// Not secret example
 resource "aws_ssm_parameter" "not_a_secret" {
   name  = join("/", [local.name_prefix, "not/a/secret", ])
   type  = "String"
